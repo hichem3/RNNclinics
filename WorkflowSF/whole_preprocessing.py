5312a10835e63089c70521c5a08e0d59966bed4a
Loic Verlingue <loicverlingue@yahoo.fr>
2019-07-28 18:25:34 +0200

    Update whole_preprocessing.py

--- WorkflowSF/whole_preprocessing.py
+++ WorkflowSF/whole_preprocessing.py
@@ -1,15 +1,19 @@
-# processing text reports, splitting train-test and saving to disk
+# processing text reports, splitting train-val-test and saving to disk
 import logging, sys
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 from clintk.utils.connection import get_engine, sql2df
 
-from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit, GridSearchCV, train_test_split
 from prescreen.vcare.reports import fetch_and_fold as rep_vc
 from prescreen.simbad.parse_reports import fetch_and_fold as rep_sb
+from prescreen.evaluation.fetch_reports import get_frames as ph12_sb
+
+# set paths
+# phase 1 from simbad and ventura care
+# expected in path :  cr_sfditep_2012.xlsx, ditep_inclus.xlsx, ditep_sf.xlsx
+table, path = 'event', '/home/v_charvet/workspace/data/'
 
 # instantiation of logger
 stdout = logging.StreamHandler(sys.stdout)
@@ -19,9 +23,6 @@ stdout.setFormatter(logging.Formatter(
 logger = logging.getLogger('default')
 logger.setLevel(logging.INFO)
 logger.addHandler(stdout)
-
-# constant variables
-#TEST_SPLIT = ..
 
 ## Fetching data
 logger.info("Loading data from Simbad and VCare")
@@ -39,10 +40,8 @@ logger.info("Target shape and head:")
 print(target.shape)
 target.head()
 
-table, path = 'event', '../data/cr_sfditep_2012.xlsx'
-
 df_rep_vc = rep_vc(table, engine, 'patient_target', 1)
-df_rep_sb = rep_sb(path, engine, 'patient_target_simbad', 1)
+df_rep_sb = rep_sb(path + '/cr_sfditep_2012.xlsx', engine, 'patient_target_simbad', 1)
 
 df_rep_sb.rename({'id': 'patient_id'}, axis=1, inplace=True)
 
@@ -50,26 +49,26 @@ df_rep = pd.concat([df_rep_vc, df_rep_sb], ignore_index=True, sort=False)
 
 df_rep = df_rep.merge(target[['id', 'screenfail']], left_on='patient_id', right_on='id')
 logging.info("Displaying head of reports dataframe")
-df_rep.head()
 
-#x_rep_raw, y_rep = df_rep['value'], df_rep['screenfail']
+#####
+# load Phase1/2 cohort from DITEP
 
-# concatenate
-#df_all=np.concatenate((df_rep['value'], df_rep['screenfail']),axis=1)
+df_ph12=ph12_sb(path)
 
-# splitting data for train/test
-#X_train, X_test, y_train, y_test = train_test_split(x_rep_raw, y_rep, test_size=TEST_SPLIT)
+df_all = pd.concat([df_rep, df_ph12], axis=0, ignore_index=True)
 
 # assign Train, Cval, Test
-df_all=df_rep.assign(Cohort=np.random.choice(["Train","Val","Test"], df_rep.shape[0], p=[0.7, 0.15, 0.15]))
-
-# follow missing values
-df_all=df_rep.assign(CompleteValues=df_all['value'].apply(len)!=0)
+df_all=df_all.assign(Cohort=np.random.choice(["Train","Val","Test"],
+                                           df_all.shape[0], p=[0.7, 0.15, 0.15]))
 
 logging.info("Loading and splitting done")
 print(df_all.shape)
 df_all.head()
+pd.value_counts(df_all['Cohort'])
+
+df_all=df_all.assign(CompleteValues=df_all['value'].apply(len)!=0)
 
 #write file
-df_all.to_csv("df_all.csv")
+df_all.to_csv(path + "/df_all.csv")
 logging.info("Writing")
+
