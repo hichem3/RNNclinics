fb003a5a9a0a299097a51bdc55f213f995d68121
Loic Verlingue <loicverlingue@yahoo.fr>
2019-08-16 12:42:25 +0200

    Update utils.py

--- WorkflowSF/utils.py
+++ WorkflowSF/utils.py
@@ -1,8 +1,14 @@
 
+import keras
+from keras import backend as K
+
 def rec_scorer(y_true, y_pred):
     '''Calculates the recall, a metric for multi-label classification of
     how many relevant items are selected.
     '''
+    import keras
+    from keras import backend as K
+    
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
